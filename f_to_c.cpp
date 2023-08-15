4d3b2df27f7afd2f188492d941e82164ab4e48d1
Raj Rawariya <98628762+Raj100@users.noreply.github.com>
2023-08-15 14:36:53 +0530

    commit #1

---
+++ f_to_c.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+  float f;
+  cin>>f;
+  float c=(f-32)*(5.0/9.0);
+  cout<<c;
+  return 0;
+}
---
+++ hello.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<< "Raj";  
+    int a;
+    cin >> a;
+    cout<< a;
+    return 0;
+}
